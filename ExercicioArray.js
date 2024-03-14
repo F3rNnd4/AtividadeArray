ff553e26b54464bdda4f86139814435e29754a13
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2024-03-14 11:23:28 -0300

    Primeiro Método

---
+++ ExercicioArray.js
@@ -0,0 +1,36 @@
+//Exercicio Array
+
+//Método .reverse()
+//----------------Exemplo 1----------------
+//Declaro elementos no caso as cores 
+let cores = ['roxo', 'branco', 'preto'];
+
+//Exibo só para comparar depois com
+// o resultado do uso do método 
+console.log('Cores iniciais: ' + cores);
+
+
+//Utilizo o método .reverse() para inverter a ordem
+//dos elementos declarados
+let coresEmReverso = cores.reverse();
+
+//Exibo as cores que declarei da forma invertida
+console.log('Cores após o método: ' + coresEmReverso);
+
+//----------------Exemplo 2----------------
+//Declaro elementos iniciais
+let petsFavoritos = ['calopsita', 'cachorro', 'gato', 'porquinho da india'];
+
+//Exibo eles para comparar com os outros resultados
+console.log('Lista não certa dos meus Pets favoritos: ' + petsFavoritos);
+
+//Uso o método para inverter a ordem
+let petsFavoritosInvertidos = petsFavoritos.reverse();
+
+//Exibo os elementos na ordem invertida
+console.log('Lista certa dos meus Pets favoritos: ' + petsFavoritosInvertidos);
+
+//Método .filter()
+//
+
+//Método .every()
