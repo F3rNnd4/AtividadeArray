ae86e0b29b88328758fd5047b6a51cfcab482ec5
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2024-03-16 09:33:55 -0300

    Segundo método

--- ExercicioArray.js
+++ ExercicioArray.js
@@ -1,36 +1,86 @@
 //Exercicio Array
 
-//Método .reverse()
+    //Método .reverse()
+//Esse método tem a função de inverter a ordem dos elementos, 
+//alterando o primeiro array.
+
 //----------------Exemplo 1----------------
-//Declaro elementos no caso as cores 
+//Declaro elementos, no caso as cores;
 let cores = ['roxo', 'branco', 'preto'];
 
 //Exibo só para comparar depois com
-// o resultado do uso do método 
+// o resultado com o método aplicado;
 console.log('Cores iniciais: ' + cores);
 
 
 //Utilizo o método .reverse() para inverter a ordem
-//dos elementos declarados
+//dos elementos declarados;
 let coresEmReverso = cores.reverse();
 
-//Exibo as cores que declarei da forma invertida
+//Exibo as cores que declarei da forma invertida;
 console.log('Cores após o método: ' + coresEmReverso);
 
 //----------------Exemplo 2----------------
-//Declaro elementos iniciais
-let petsFavoritos = ['calopsita', 'cachorro', 'gato', 'porquinho da india'];
+//Declaro elementos iniciais;
+let pets = ['calopsita', 'cachorro', 'gato', 'porquinho da india'];
 
-//Exibo eles para comparar com os outros resultados
-console.log('Lista não certa dos meus Pets favoritos: ' + petsFavoritos);
+//Exibo eles para comparar com os outros resultados;
+console.log(pets);
 
-//Uso o método para inverter a ordem
-let petsFavoritosInvertidos = petsFavoritos.reverse();
+//Uso o método para inverter a ordem;
+let petsInvertidos = pets.reverse();
 
-//Exibo os elementos na ordem invertida
-console.log('Lista certa dos meus Pets favoritos: ' + petsFavoritosInvertidos);
+//Exibo os elementos na ordem invertida;
+console.log(petsInvertidos);
 
-//Método .filter()
+    //Método .concat()
+//Este método permite juntar arrays e retornar um novo
+
+//----------------Exemplo 1----------------
+//Crio o meu array, a minha lista;
+let listaNumeros = [1, 2, 3];
+//Crio um novo array com o método para adicionar mais números a essa lista;
+let novaListaNumeros = listaNumeros.concat([4, 5, 6]);
+
+//Exibo a lista inicial e a final atualizada;
+console.log(listaNumeros);
+console.log(novaListaNumeros);
+
+//----------------Exemplo 2----------------
+//Crio o array, uma lista de frutas
+let frutasVitaminaC = ['acerola', 'laranja', 'limão'];
+//Novo array utilizando o método para colocar mais frutas;
+let novasFrutasVitamicaC = frutasVitaminaC.concat(['abacaxi', 'caju', 'morango']);
+
+//Exibo a lista original e a final
+console.log(frutasVitaminaC);
+console.log(novasFrutasVitamicaC);
+
+    //Método .join()
 //
 
-//Método .every()
+    //Método .indexOf()
+//
+
+    //Método .includes()
+//
+
+    //Método .forEach()
+//
+
+    //Método .filter()
+//
+
+    // Método .lastIndex()
+//
+
+    //Método .reduce()
+//
+
+    //Método .slice()
+//
+
+
+
+    //Método .splice()
+//
